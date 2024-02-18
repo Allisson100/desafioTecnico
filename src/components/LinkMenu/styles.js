8f9cbf1e41e4afbfe08440090979df16eb2c6ee1
Allisson100 <allisson.boiane@outlook.com>
2024-02-18 18:07:00 -0300

    Refatora tamanho da font no menu

--- src/components/LinkMenu/styles.js
+++ src/components/LinkMenu/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const LinkContainerStyled = styled(Link)`
     color: ${props => props.theme.fontColors.main};
-    font-size: 1.3rem;
+    font-size: 1rem;
     text-align: center;
     text-decoration: ${props => props.$underline ? "underline" : "none"};
     text-transform: capitalize;
